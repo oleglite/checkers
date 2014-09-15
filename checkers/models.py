cfea838f7d0c4892ef156df65dafcaf22cfeed12
Oleg Beloglazov <beloglazov.oleg@gmail.com>
2014-09-15 19:27:28 +0300

    add move check logic

--- checkers/models.py
+++ checkers/models.py
@@ -15,31 +15,68 @@ class Board(object):
                 return checker
         return None
 
-    def is_move_allowed(self, checker, x, y):
-        if not (0 <= x <= 7 and 0 <= y <= 7):
-            return False
-
-        if self.get_checker_in_position(x, y) or not self.is_black_field(x, y):
-            return False
-
-        if checker.is_king():
-            pass
-        else:
-            if checker.color == Checker.WHITE:
-                # ход вперед
-                if y > checker.y:
-                    return False
-
-                # бить вперед
-                # бить назад
-
     def is_black_field(self, x, y):
         return (x + y + 1) % 2
+
+
+class Move(object):
+    class Type:
+        WRONG = 0
+        MOVE = 1
+        KICK = 2
+
+    def __init__(self, move_type, victim=None):
+        self.move_type = move_type
+        self.victim = victim
 
 
 class BoardManager(object):
     def __init__(self, board):
         self.board = board
+
+    def get_move(self, checker, x, y):
+        if not (0 <= x <= 7 and 0 <= y <= 7):
+            return Move(Move.Type.WRONG)
+
+        if self.board.get_checker_in_position(x, y) or not self.board.is_black_field(x, y):
+            return Move(Move.Type.WRONG)
+
+        dx = x - checker.x
+        dy = y - checker.y
+        x_direction = 1 if dx > 0 else -1
+        y_direction = 1 if dy > 0 else -1
+
+        if checker.is_king():
+            if abs(dx) != abs(dy):
+                return Move(Move.Type.WRONG)
+
+            victims = []
+            for i in xrange(1, abs(dx)):
+                victim = self.board.get_checker_in_position(checker.x + i * x_direction, checker.y + i * y_direction)
+                if victim:
+                    victims.append(victim)
+
+            if len(victims) == 1:
+                return Move(Move.Type.KICK, victims[0])
+
+            return Move(Move.Type.MOVE)
+
+        else:
+            # kick
+            if abs(dx) == abs(dy) == 2:
+                victim = self.board.get_checker_in_position(checker.x + x_direction, checker.y + y_direction)
+                if victim and victim.color != checker.color:
+                    return Move(Move.Type.KICK, victim)
+
+            # check direction
+            if checker.color == Checker.WHITE and dy < 0 or checker.color == Checker.BLACK and dy > 0:
+                return Move(Move.Type.WRONG)
+
+            # move forward
+            if abs(dx) == abs(dy) == 1:
+                return Move(Move.Type.MOVE)
+
+            return Move(Move.Type.WRONG)
 
 
 class Checker(object):
