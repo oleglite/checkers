254f848a81dc18e7e4305368060d51f7caf0ba40
Oleg Beloglazov <beloglazov.oleg@gmail.com>
2014-11-18 09:16:58 +0300

    fix ai

--- checkers/ai.py
+++ checkers/ai.py
@@ -8,19 +8,12 @@ from checkers.logic import get_move, Move, get_available_moves_all_checkers
 MAX_RECURSION = 3
 
 
-class RESULT:
-    WIN = 4
-    UNKNOWN = 3
-    DRAW = 2
-    LOSE = 1
-    SKIP = 0
-
-
 def calculate_ai_move(board, ai_color, player_color):
-    move_coords, result_tuple = minimax(board, ai_color, player_color)
-    if result_tuple[0] == RESULT.SKIP:
-        return None
-    return move_coords
+    results = minimax(board, ai_color, player_color)
+    result = choice(results)
+    for r in results:
+        print r
+    return result.move_coords[0]
 
 
 def minimax(board, ai_color, player_color, recursion_level=0):
@@ -30,6 +23,9 @@ def minimax(board, ai_color, player_color, recursion_level=0):
     result_tuple = (result, ai_kicks, -player_kicks)
     """
     recursion_level += 1
+
+    if recursion_level == MAX_RECURSION:
+        return [Result(Result.TYPE.UNKNOWN)]
 
     color = ai_color if recursion_level % 2 else player_color
 
@@ -47,31 +43,63 @@ def minimax(board, ai_color, player_color, recursion_level=0):
 
             winner = board_copy.get_winner()
             if winner:
-                if color == ai_color:
-                    return checker.x, checker.y, field[0], field[1], (RESULT.WIN, 0, 0)
-                else:
-                    return checker.x, checker.y, field[0], field[1], (RESULT.LOSE, 0, 0)
+                result_type = Result.TYPE.WIN if color == ai_color else Result.TYPE.LOSE
+                return [Result(result_type, (checker.x, checker.y, field[0], field[1]))]
 
-            if recursion_level == MAX_RECURSION:
-                result = RESULT.UNKNOWN
-                ai_kicks = player_kicks = 0
-            else:
-                result, ai_kicks, player_kicks = minimax(board_copy, ai_color, player_color, recursion_level)[1]
+            turn_results = minimax(board_copy, ai_color, player_color, recursion_level)
 
+            turn = Result.TURN.NONE
             if move.type == Move.TYPE.KICK:
-                if color == ai_color:
-                    ai_kicks += 1
-                else:
-                    player_kicks -= 1
+                turn = Result.TURN.AI_KICK if color == ai_color else Result.TURN.PLAYER_KICK
 
-            results.append((move_coords, (result, ai_kicks, player_kicks)))
+            for result in turn_results:
+                result.add_turn(move_coords, turn)
+            results.extend(turn_results)
 
     if not results:
-        return (), (RESULT.SKIP, 0, 0)
+        return [Result(Result.TYPE.SKIP)]
+
+    good_results = [r for r in results if r.score_ai > r.score_player]
+    if good_results:
+        results = good_results
 
     fn = max if color == ai_color else min
-    best_result = fn(results, key=lambda i: i[1])
-    best_results = [result for result in results if result[1] == best_result[1]]
-    ai_choice = choice(best_results)
-    return ai_choice
+    best_result = fn(results, key=lambda r: r.key())
+    best_results = [result for result in results if result.key() == best_result.key()]
+    return best_results
 
+
+class Result(object):
+    class TYPE:
+        WIN = 4
+        UNKNOWN = 3
+        DRAW = 2
+        LOSE = 1
+        SKIP = 0
+
+    class TURN:
+        AI_KICK = 1
+        PLAYER_KICK = -1
+        NONE = 0
+
+    def __init__(self, result_type, move_coords=None):
+        self.result_type = result_type
+        self.move_coords = []
+        self.score_ai = 0
+        self.score_player = 0
+
+        if move_coords:
+            self.add_turn(move_coords, 0, 0)
+
+    def add_turn(self, move_coords, turn=None):
+        self.move_coords.insert(0, move_coords)
+        if turn == Result.TURN.AI_KICK:
+            self.score_ai += 1
+        elif turn == Result.TURN.PLAYER_KICK:
+            self.score_player += 1
+
+    def key(self):
+        return self.result_type, self.score_ai, -self.score_player
+
+    def __str__(self):
+        return '%d:%d:%d %s' % (self.result_type, self.score_ai, self.score_player, self.move_coords)
